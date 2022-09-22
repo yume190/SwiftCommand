f82e9d3d65493aac2e5f819fb7f69cdaeb306bf5
Yume <yume190@gmail.com>
2022-09-22 09:36:29 +0800

    Update FilePath+extension.swift
    `URL.init(filePath)` can only be used in macOS 13 or higher.

--- Sources/SwiftCommand/FilePath+extension.swift
+++ Sources/SwiftCommand/FilePath+extension.swift
@@ -10,14 +10,6 @@ extension FilePath {
 
     /// Creates a `Foundation.URL` from this `FilePath`.
     public var url: URL {
-#if canImport(Darwin) && swift(>=5.7)
-        if #available(macOS 13.0, *) {
-            return .init(filePath: self.string)
-        } else {
-            return .init(fileURLWithPath: self.string)
-        }
-#else
         .init(fileURLWithPath: self.string)
-#endif
     }
 }
